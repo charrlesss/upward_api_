d967a0516dbeca102d4d28e971aa79fef23a5d95
charrlesss <charlespalencia21@gmail.com>
2025-01-28 11:51:40 +0800

    f

--- src/controller/Task/Accounting/return-checks.ts
+++ src/controller/Task/Accounting/return-checks.ts
@@ -216,6 +216,103 @@ ReturnCheck.post("/get-check-list", async (req, res) => {
     });
   }
 });
+
+ReturnCheck.get("/return-checks/generate-id", async (req, res) => {
+  try {
+    const yyMM = format(new Date(), "yyMM");
+    const rows: any = await __executeQuery(
+      `
+      SELECT CONCAT(
+        SUBSTRING(RC_No, 1, LENGTH(RC_No) - 8), 
+        LEFT(RIGHT(RC_No, 8), 4), 
+        '-', 
+        LPAD(CAST(RIGHT(RC_No, 3) AS UNSIGNED) + 1, 3, '0')
+      ) AS NewRefCode
+      FROM Return_Checks
+      WHERE LEFT(RIGHT(RC_No, 8), 4) = DATE_FORMAT(CURDATE(), '%y%m')
+      GROUP BY RC_No
+      ORDER BY CAST(RIGHT(RC_No, 3) AS UNSIGNED) DESC
+      LIMIT 1;
+      `,
+      req
+    );
+
+    let newRefCode = `${yyMM}-001`;
+
+    if (rows.length > 0) {
+      newRefCode = rows[0].NewRefCode;
+    }
+
+    res.send({
+      message: "Successfully Get Check List",
+      success: true,
+      newRefCode,
+    });
+  } catch (error: any) {
+    console.log(error.message);
+    res.send({
+      message: `We're experiencing a server issue. Please try again in a few minutes. If the issue continues, report it to IT with the details of what you were doing at the time.`,
+      success: false,
+      newRefCode: "",
+    });
+  }
+});
+
+ReturnCheck.post("/return-checks/return-checks-search", async (req, res) => {
+  try {
+    const qry = `
+      SELECT 
+      date_format(RC_Date,'%M %d %Y') AS RefDate,
+      RC_No AS RefNo,
+      Explanation FROM Return_Checks
+      WHERE (Left(Explanation,7)<>'-- Void') AND 
+      (RC_No LIKE '%${req.body.search}%' OR Explanation LIKE '%${req.body.search}%')
+      GROUP BY RC_Date, RC_No, Explanation ORDER BY RC_Date
+      `;
+    console.log(qry);
+    res.send({
+      message: `Successfully Search Return Checks.`,
+      success: true,
+      data: await __executeQuery(qry, req),
+    });
+  } catch (error: any) {
+    res.send({
+      message: `We're experiencing a server issue. Please try again in a few minutes. If the issue continues, report it to IT with the details of what you were doing at the time.`,
+      success: false,
+      data: [],
+    });
+  }
+});
+
+ReturnCheck.post(
+  "/return-checks/return-checks-search-selected",
+  async (req, res) => {
+    try {
+      const qry1 = `SELECT * FROM Return_Checks WHERE RC_No = '${req.body.RefNo}' ORDER BY nSort`;
+      const qry2 = `SELECT * FROM Journal WHERE Source_Type = 'RC' and Source_No = '${req.body.RefNo}'`;
+      const data1 = await __executeQuery(qry1, req);
+      const data2 = await __executeQuery(qry2, req);
+
+      const replacer = (key:any, value:any) => (typeof value === 'bigint' ? value.toString() : value);
+
+      res.send({
+        message: `Successfully Search Return Checks.`,
+        success: true,
+        data1: JSON.stringify(data1, replacer),
+        data2: JSON.stringify(data2, replacer),
+      });
+    } catch (error: any) {
+      console.log(error.message);
+      res.send({
+        message: `We're experiencing a server issue. Please try again in a few minutes. If the issue continues, report it to IT with the details of what you were doing at the time.`,
+        success: false,
+        data1: [],
+        data2: [],
+      });
+    }
+  }
+);
+
 ReturnCheck.post("/return-checks/save", async (req, res) => {
   try {
     const BranchCode = "HO";
@@ -257,7 +354,7 @@ ReturnCheck.post("/return-checks/save", async (req, res) => {
           req.body.explanation,
           selCheckItm[4],
           selCheckItm[3],
-          parseFloat(selCheckItm[6].toString().replace(/,/g,'')),
+          parseFloat(selCheckItm[6].toString().replace(/,/g, "")),
           selCheckItm[9],
           selCheckItm[7],
           selCheckItm[5],
@@ -329,7 +426,7 @@ ReturnCheck.post("/return-checks/save", async (req, res) => {
         [
           BranchCode,
           req.body.date,
-          'RC',
+          "RC",
           req.body.refNo,
           req.body.explanation,
           accountingEntrItm[0],
@@ -339,8 +436,8 @@ ReturnCheck.post("/return-checks/save", async (req, res) => {
           accountingEntrItm[4],
 
           accountingEntrItm[5],
-          parseFloat(accountingEntrItm[2].toString().replace(/,/g,'')),
-          parseFloat(accountingEntrItm[3].toString().replace(/,/g,'')),
+          parseFloat(accountingEntrItm[2].toString().replace(/,/g, "")),
+          parseFloat(accountingEntrItm[3].toString().replace(/,/g, "")),
           accountingEntrItm[10],
           accountingEntrItm[8],
           accountingEntrItm[9],
@@ -349,8 +446,8 @@ ReturnCheck.post("/return-checks/save", async (req, res) => {
           accountingEntrItm[15],
           accountingEntrItm[12],
 
-          'RTC',
-          ''
+          "RTC",
+          "",
         ],
         req
       );
@@ -375,47 +472,182 @@ ReturnCheck.post("/return-checks/save", async (req, res) => {
     });
   }
 });
-ReturnCheck.get("/return-checks/generate-id", async (req, res) => {
+ReturnCheck.post("/return-checks/update", async (req, res) => {
   try {
-    const yyMM = format(new Date(), "yyMM");
-    const rows: any = await __executeQuery(
-      `
-      SELECT CONCAT(
-        SUBSTRING(RC_No, 1, LENGTH(RC_No) - 8), 
-        LEFT(RIGHT(RC_No, 8), 4), 
-        '-', 
-        LPAD(CAST(RIGHT(RC_No, 3) AS UNSIGNED) + 1, 3, '0')
-      ) AS NewRefCode
-      FROM Return_Checks
-      WHERE LEFT(RIGHT(RC_No, 8), 4) = DATE_FORMAT(CURDATE(), '%y%m')
-      GROUP BY RC_No
-      ORDER BY CAST(RIGHT(RC_No, 3) AS UNSIGNED) DESC
-      LIMIT 1;
-      `,
+    const { userAccess }: any = await VerifyToken(
+      req.cookies["up-ac-login"] as string,
+      process.env.USER_ACCESS as string
+    );
+
+    if (userAccess.includes("ADMIN")) {
+      return res.send({
+        message: `CAN'T UPDATE, ADMIN IS FOR VIEWING ONLY!`,
+        success: false,
+      });
+    }
+
+
+    if (!(await saveUserLogsCode(req, "update", req.body.refNo, "Return Checks"))) {
+      return res.send({ message: "Invalid User Code", success: false });
+    }
+
+    const BranchCode = "HO";
+    const BranchName = "Head Office";
+    await __executeQuery(
+      `DELETE FROM Return_Checks WHERE RC_No ='${req.body.refNo}'`,
       req
     );
 
-    let newRefCode = `${yyMM}-001`;
+    req.body.dgvSelChecks.forEach(async (selCheckItm: any, index: number) => {
+      const insertQuery = `
+      INSERT INTO \`return_checks\`
+      (\`Area\`,
+      \`RC_Date\`,
+      \`RC_No\`,
+      \`Explanation\`,
+      \`Check_No\`,
+      \`Date_Deposit\`,
+       \`Amount\`,
+       \`Reason\`,
+       \`Bank\`,
+       \`Check_Date\`,
+       \`Date_Return\`,
+       \`SlipCode\`,
+       \`ORNum\`,
+       \`BankAccnt\`,
+       \`nSort\`,
+      \`Date_Collect\`,
+      \`Temp_RCNo\`)
+      VALUES
+      (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?);
+    `;
+      await __executeQueryWithParams(
+        insertQuery,
+        [
+          BranchCode,
+          req.body.date,
+          req.body.refNo,
+          req.body.explanation,
+          selCheckItm[4],
+          selCheckItm[3],
+          parseFloat(selCheckItm[6].toString().replace(/,/g, "")),
+          selCheckItm[9],
+          selCheckItm[7],
+          selCheckItm[5],
+          selCheckItm[10],
+          selCheckItm[2],
+          selCheckItm[0],
+          selCheckItm[8],
+          (index + 1).toString().padStart(2, "00"),
+          selCheckItm[1],
+          `${req.body.refNo}${(parseInt(req.body.refNo.split("-")[1]) + index)
+            .toString()
+            .padStart(2, "0")}`,
+        ],
+        req
+      );
 
-    if (rows.length > 0) {
-      newRefCode = rows[0].NewRefCode;
+      await __executeQueryWithParams(
+        `UPDATE PDC SET SlipCode = '', ORNum = '' WHERE Check_No = ?`,
+        [selCheckItm[4]],
+        req
+      );
+
+      // Update Journal
+      await __executeQueryWithParams(
+        `UPDATE Journal SET TC = 'RTC' WHERE Check_No = ? AND Source_No = ? AND Source_Type = 'OR'`,
+        [selCheckItm[4], selCheckItm[0]],
+        req
+      );
+    });
+
+    await __executeQueryWithParams(
+      `DELETE FROM Journal WHERE Source_No = ? AND Source_Type = 'RC'`,
+      [req.body.refNo],
+      req
+    );
+
+    for (const accountingEntrItm of req.body.dgvAccountingEntry) {
+      const journalQuery = `
+      INSERT INTO \`journal\`
+      (\`Branch_Code\`,
+      \`Date_Entry\`,
+
+      \`Source_Type\`,
+      \`Source_No\`,
+      \`Explanation\`,
+      \`GL_Acct\`,
+      \`cGL_Acct\`,
+      \`Sub_Acct\`,
+      \`cSub_Acct\`,
+      \`ID_No\`,
+      \`cID_No\`,
+      \`Debit\`,
+
+      \`Credit\`,
+      \`Check_Date\`,
+      \`Check_No\`,
+      \`Check_Bank\`,
+      \`Check_Return\`,
+      \`Check_Deposit\`,
+      \`Check_Collect\`,
+      \`Check_Reason\`,
+      \`TC\`,
+      \`Source_No_Ref_ID\`)
+      VALUES
+      (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?);
+    `;
+      await __executeQueryWithParams(
+        journalQuery,
+        [
+          BranchCode,
+          req.body.date,
+          "RC",
+          req.body.refNo,
+          req.body.explanation,
+          accountingEntrItm[0],
+          accountingEntrItm[1],
+          BranchCode,
+          BranchName,
+          accountingEntrItm[4],
+
+          accountingEntrItm[5],
+          parseFloat(accountingEntrItm[2].toString().replace(/,/g, "")),
+          parseFloat(accountingEntrItm[3].toString().replace(/,/g, "")),
+          accountingEntrItm[10],
+          accountingEntrItm[8],
+          accountingEntrItm[9],
+          accountingEntrItm[11],
+          accountingEntrItm[14],
+          accountingEntrItm[15],
+          accountingEntrItm[12],
+
+          "RTC",
+          "",
+        ],
+        req
+      );
     }
 
+    await saveUserLogs(
+      req,
+      req.body.refNo,
+      req.body.mode === "add" ? "edit" : "update",
+      "Return Check"
+    );
     res.send({
-      message: "Successfully Get Check List",
+      message: "Successfully Save Return Check",
       success: true,
-      newRefCode,
     });
   } catch (error: any) {
     console.log(error.message);
     res.send({
       message: `We're experiencing a server issue. Please try again in a few minutes. If the issue continues, report it to IT with the details of what you were doing at the time.`,
       success: false,
-      newRefCode: "",
+      checkList: [],
     });
   }
 });
-
 //// old ===============
 
 ReturnCheck.get("/get-new-return-check-id", async (req, res) => {
